a60ac5311b10b86a1493fac5a665a06dc9fc23f2
J-Dierberger-Laptop <jdierberger1@gmail.com>
2018-09-05 20:45:57 -0400

    Changes to Person 4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Josh
  *  @version 1.1
  */
 public class Person4 {
@@ -30,7 +30,10 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
+      String s = "";
+      for (int i = 0; i < input.length(); i++) {
+        s += (char)(input.charAt(i) + 1);
+      }
       return null;
     }
     
